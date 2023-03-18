19897b52a9310a22bc9633d3a699fe183fe293fe
Michael Cuffaro <michael@cuffaro.com>
2023-03-18 07:55:50 +0100

    feat: add sts

---
+++ README.md
@@ -0,0 +1,4 @@
+# Kubernetes workshop
+
+
+
--- webapp-color/app.py
+++ webapp-color/app.py
@@ -1,9 +1,9 @@
-from flask import Flask
-from flask import render_template
-import socket
-import random
-import os
 import argparse
+import os
+import random
+import socket
+
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
@@ -13,54 +13,78 @@ color_codes = {
     "blue": "#2980b9",
     "blue2": "#30336b",
     "pink": "#be2edd",
-    "darkblue": "#130f40"
+    "darkblue": "#130f40",
+    # "yellow": "#ffff00",
 }
 
 SUPPORTED_COLORS = ",".join(color_codes.keys())
 
 # Get color from Environment variable
-COLOR_FROM_ENV = os.environ.get('APP_COLOR')
+COLOR_FROM_ENV = os.environ.get("APP_COLOR")
 # Generate a random color
-COLOR = random.choice(["red", "green", "blue", "blue2", "darkblue", "pink"])
+COLOR = random.choice(["red", "green", "blue", "blue2", "darkblue", "pink", "yellow"])
+# COLOR = random.choice(["yellow"])
 
 
 @app.route("/")
 def main():
-    # return 'Hello'
-    return render_template('hello.html', name=socket.gethostname(), color=color_codes[COLOR])
+    return render_template(
+        "hello.html", name=socket.gethostname(), color=color_codes[COLOR]
+    )
 
 
 if __name__ == "__main__":
 
-    print(" This is a sample web application that displays a colored background. \n"
-          " A color can be specified in two ways. \n"
-          "\n"
-          " 1. As a command line argument with --color as the argument. Accepts one of " + SUPPORTED_COLORS + " \n"
-          " 2. As an Environment variable APP_COLOR. Accepts one of " + SUPPORTED_COLORS + " \n"
-          " 3. If none of the above then a random color is picked from the above list. \n"
-          " Note: Command line argument precedes over environment variable.\n"
-          "\n"
-          "")
+    print(
+        " This is a sample web application that displays a colored background. \n"
+        " A color can be specified in two ways. \n"
+        "\n"
+        " 1. As a command line argument with --color as the argument. Accepts one of "
+        + SUPPORTED_COLORS
+        + " \n"
+        " 2. As an Environment variable APP_COLOR. Accepts one of "
+        + SUPPORTED_COLORS
+        + " \n"
+        " 3. If none of the above then a random color is picked from the above list. \n"
+        " Note: Command line argument precedes over environment variable.\n"
+        "\n"
+        ""
+    )
 
     # Check for Command Line Parameters for color
     parser = argparse.ArgumentParser()
-    parser.add_argument('--color', required=False)
+    parser.add_argument("--color", required=False)
     args = parser.parse_args()
 
     if args.color:
         print("Color from command line argument =" + args.color)
         COLOR = args.color
         if COLOR_FROM_ENV:
-            print("A color was set through environment variable -" + COLOR_FROM_ENV + ". However, color from command line argument takes precendence.")
+            print(
+                "A color was set through environment variable -"
+                + COLOR_FROM_ENV
+                + ". However, color from command line argument takes precendence."
+            )
     elif COLOR_FROM_ENV:
-        print("No Command line argument. Color from environment variable =" + COLOR_FROM_ENV)
+        print(
+            "No Command line argument. Color from environment variable ="
+            + COLOR_FROM_ENV
+        )
         COLOR = COLOR_FROM_ENV
     else:
-        print("No command line argument or environment variable. Picking a Random Color =" + COLOR)
+        print(
+            "No command line argument or environment variable. Picking a Random Color ="
+            + COLOR
+        )
 
     # Check if input color is a supported one
     if COLOR not in color_codes:
-        print("Color not supported. Received '" + COLOR + "' expected one of " + SUPPORTED_COLORS)
+        print(
+            "Color not supported. Received '"
+            + COLOR
+            + "' expected one of "
+            + SUPPORTED_COLORS
+        )
         exit(1)
 
     # Run Flask Application
